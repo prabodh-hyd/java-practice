6393638b08039cd20f3967b43bc87a1b57e2e6cd
Rama <99578648+Ramanaresh1@users.noreply.github.com>
2023-02-14 18:23:47 +0530

    java revision

---
+++ AbstractNonAccessMod.java
@@ -0,0 +1,23 @@
+
+    // abstract class
+    abstract class abstract_gfg{
+        abstract void myMethod();
+    }
+
+    //extending abstract class
+    class MyClass extends abstract_gfg{
+
+        // overriding abstract method otherwise
+        // code will produce error
+        void myMethod(){
+            System.out.println("GeeksforGeeks");
+        }
+    }
+    class AbstractNonAccessMod {
+        public static void main(String[] args) {
+            MyClass obj=new MyClass();
+            obj.myMethod();
+        }
+    }
+
+
