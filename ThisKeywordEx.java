ad5dcf29485fe9324a8e091588244b601e9342cf
Rama <99578648+Ramanaresh1@users.noreply.github.com>
2023-02-17 16:40:10 +0530

    this keyword example

--- ThisKeywordEx.java
+++ ThisKeywordEx.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ThisKeywordEx {
-}
+public class ThisKeywordEx {
+        // this is used to access methods of the current class.it is a resrved word in java.
+    //we cannt use its as an identifier.
+        int a = 10;   // instance variable
+
+        // static variable
+        static int b = 20;
+
+        void GFG()
+        {
+            // referring current class(i.e, class RR)
+            // instance variable(i.e, a)
+            this.a = 100;
+
+            System.out.println(a);
+
+            // referring current class(i.e, class RR)
+            // static variable(i.e, b)
+            this.b = 600;
+
+            System.out.println(b);
+        }
+
+        public static void main(String[] args)
+        {
+
+            new ThisKeywordEx().GFG();
+        }
+    }
+
